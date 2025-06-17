8f757a47fb19a374e4eb89327c3cb1d189a026c2
JonHedegaardNielsen <166702823+JonHedegaardNielsen@users.noreply.github.com>
2025-06-17 14:11:55 +0200

    Fixed name

---
+++ Frontend/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import {ref} from 'vue'
+import Person from './components/Person.vue'
+
+const people = ref([
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+  {firstName: 'Jon', lastName: 'Nielsen', age: 18, gender: 'Male',  height: 200, weight: 110},
+])
+
+</script>
+
+<template>
+  <div id="peopleContainer">
+    <div v-for="person in people" class="personContainer">
+      <Person 
+          :firstName="person.firstName"
+          :lastName="person.lastName"
+          :age="person.age"
+          :gender="person.gender"
+          :height="person.height"
+          :weight="person.weight">
+      </Person>
+    </div>
+  
+  </div>
+</template>
+
+<style scoped>
+  div#peopleContainer{
+    width: 100%;
+    height: 100%;
+    background-color: aqua;
+  }
+  div.personContainer{
+    display: inline-block;
+    margin-left: 7px;
+    margin-right: 7px;
+  }
+
+  template{
+    background-color: black;
+  }
+</style>
---
+++ Frontend/src/components/Person.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import {ref} from 'vue';
+    defineProps(['firstName', 'lastName', 'age', 'gender', 'height', 'weight'])
+
+</script>
+
+<template>
+    <div id="personBox">
+        <img src="../assets/NoImage.png" alt="">
+        <span>{{firstName}} {{ lastName }} <br></span>
+        <span>age: {{age}} <br></span>
+        <span>{{ gender }} <br></span>
+        <span>{{ height }} <br></span>
+        <span>{{ weight }} <br></span>
+    </div>
+</template>
+
+<style scoped>
+div#personBox{
+    background-color: black;
+    width: 150px;
+    border-radius: 5px;
+}
+#personBox img{
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    height: 60%;
+    width: 100%;
+    background-color: blue;
+}
+</style>
